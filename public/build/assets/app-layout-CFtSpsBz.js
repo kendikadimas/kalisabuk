9560601d10bba87b354d30445092eb9a0577b26a
kendikadimas <dimas.fazrulfalah@mhs.unsoed.ac.id>
2026-02-05 07:54:17 +0700

    fix iframe

---
+++ public/build/assets/app-layout-CFtSpsBz.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-DdSQCcPU.js";import{A as t}from"./app-sidebar-layout-CmCcbV2n.js";const i=({children:a,breadcrumbs:o,...r})=>s.jsx("div",{className:"bg-slate-50 min-h-screen",children:s.jsx(t,{breadcrumbs:o,...r,children:a})});export{i as A};
